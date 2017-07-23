a1e08893aadb285a6be5e0c944f578120b6aee82
Raymond <nomyards@gmail.com>
2017-07-23 16:35:40 -0400

    Completed homework

--- exercises.js
+++ exercises.js
@@ -3,6 +3,7 @@
 function getBiggest(x, y) {
   //x and y are integers.  Return the larger integer
   //if they are the same return either one
+  return Math.max(x,y);
 }
 
 function greeting(language) {
@@ -11,15 +12,34 @@ function greeting(language) {
   //language: 'English' -> 'Hello!'
   //language: 'Spanish' -> 'Hola!'
   //if language is undefined return 'Hello!'
+  if (language ==='German'){
+  return ('Guten Tag!');
+  } else if (language ==='English'){
+  return ('Hello!');
+  } else if (language ==='Spanish'){
+  return ('Hola!');
+  } else {
+  return ('Hello!');
+  }
 }
 
 function isTenOrFive(num) {
   //return true if num is 10 or 5
   //otherwise return false
+  if (num === 10 || num === 5){
+  return true;
+  } else {
+  return false;
+  }
 }
 
 function isInRange(num) {
   //return true if num is less than 50 and greater than 20
+  if (num < 50 && num > 20){
+  return true;
+  } else {
+  return false;
+  }
 }
 
 function isInteger(num) {
@@ -29,6 +49,11 @@ function isInteger(num) {
   //-10 -> true
   //otherwise return false
   //hint: you can solve this using Math.floor
+  if (num === Math.floor(num)){
+  return true;
+  } else {
+  return false;
+  }
 }
 
 function fizzBuzz(num) {
@@ -36,6 +61,15 @@ function fizzBuzz(num) {
   //if num is divisible by 5 return 'buzz'
   //if num is divisible by 3 & 5 return 'fizzbuzz'
   //otherwise return num
+  if (num % 3 == 0 && num % 5 == 0) {
+  return 'fizzbuzz';
+  } else if (num % 3 == 0) {
+  return 'fizz';
+  } else if (num % 5 == 0){
+  return 'buzz';
+  } else {
+  return num;
+  }
 }
 
 function isPrime(num) {
@@ -44,62 +78,102 @@ function isPrime(num) {
   //hint: a prime number is only evenly divisible by itself and 1
   //hint2: you can solve this using a for loop
   //note: 0 and 1 are NOT considered prime numbers
-}
+  for (var i = 2; i < num; i++) {
+  if(num % i === 0) {
+  return false;
+  }
+  }
+  return num > 1;
+  }
+
 
 function returnFirst(arr) {
   //return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   //return the last item of the array
+  return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
   //return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   //arr is an array of integers  
   //increase each integer by one
   //return the array
+  for (var i = 0; i < arr.length; i++){
+  arr[i] += 1;
+}
+  return arr;
+
+
 }
 
 function addItemToArray(arr, item) {
   //add the item to the end of the array
   //return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   //add the item to the front of the array
   //return the array
   //hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
   //words is an array of strings
   //return a string that is all of the words concatenated together
   //spaces need to be between each word
-  //example: ['Hello', 'world!'] -> 'Hello world!'
+  //example: ['Hello', 'world!'] -> 'Hello world!
+  return words.join(' ');
 }
 
 function contains(arr, item) {
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
+for (var i = 0; i < arr.length; i++){
+if (arr[i] === item){
+return true;
+}
+}
+return false;
 }
 
 function addNumbers(numbers) {
   //numbers is an array of integers.
   //add all of the integers and return the value
+var total = 0;
+for(var i = 0; i < numbers.length; i++) {
+total += numbers[i];
+}
+return total;
 }
 
 function averageTestScore(testScores) {
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
+var total = 0;
+var test_num = testScores.length;
+for (var i = 0; i < testScores.length; i++){
+total += testScores[i];
+}
+return total / test_num;
 }
 
 function largestNumber(numbers) {
   //numbers is an array of integers
   //return the largest integer
+var largest = Math.max.apply(0, numbers);
+return largest;
 }
 
 //Do not modify code below this line.
